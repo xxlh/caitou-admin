164111184180ce6c50f6a5ee6e4aeda5e4fe607c
Esone <esone.qiu@ringcentral.com>
2020-04-29 01:07:41 +0800

    fix signin fail(no response)  by remove empty Authorization header  due to `RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]` on server .htaccess

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,18 +3,22 @@
  * 更详细的 api 文档: https://github.com/umijs/umi-request
  */
 import { extend } from 'umi-request';
-import { httpErrorHandler } from './requestError'  // 移至dva最上层处理。目的为了在effect内如果做catch可以阻止dva上层统一错误捕获
+import { httpErrorHandler } from './requestError'; // 移至dva最上层处理。目的为了在effect内如果做catch可以阻止dva上层统一错误捕获
 
 /**
  * 配置request请求时的默认参数
  */
+/* global BASE_URL */
+const token: string = localStorage.getItem('token') || '';
 const request = extend({
   prefix: BASE_URL,
   errorHandler: httpErrorHandler, // 默认错误处理
   credentials: 'include', // 默认请求是否带上cookie
-  headers: {
-    Authorization: localStorage.getItem("token") || ''
-  },
+  headers: token
+    ? {
+        Authorization: token,
+      }
+    : {},
 });
 
 export default request;
