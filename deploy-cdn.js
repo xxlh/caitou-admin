0b3d1898133636163ae705e1139f47604eac100f
Esone <01@eexx.me>
2021-04-23 21:39:32 +0800

    update project info

--- deploy-cdn.js
+++ deploy-cdn.js
@@ -2,20 +2,21 @@ var WQCLOUD = require('wqcloud');
 var config = require('./config/deploy.config');
 
 var options = {
-	SecretId: config.SecretId,
-	SecretKey: config.SecretKey,
-	// 不填，每次请求都会自动重新生成
-	// Signature: '',
-	Nonce: parseInt(Math.random() * 999999, 10),
-	Timestamp: parseInt(new Date() / 1000, 10),
-	Region: '',
-	Version: '2018-06-06',
-	'Paths.0': 'https://property.ieexx.com/',
-	FlushType: 'delete'
+  SecretId: config.SecretId,
+  SecretKey: config.SecretKey,
+  // 不填，每次请求都会自动重新生成
+  // Signature: '',
+  Nonce: parseInt(Math.random() * 999999, 10),
+  Timestamp: parseInt(new Date() / 1000, 10),
+  Region: '',
+  Version: '2018-06-06',
+  'Paths.0': 'https://caitou.ieexx.com/',
+  FlushType: 'delete',
 };
 
 var CDN = WQCLOUD.CDN(options);
-CDN.PurgePathCache().then((res) => {
-	if (!res.Response.Error && res.Response.TaskId) console.info('成功刷新CDN缓存！TaskId: ' + res.Response.TaskId);
-	else console.error(res.Response.Error);
+CDN.PurgePathCache().then(res => {
+  if (!res.Response.Error && res.Response.TaskId)
+    console.info('成功刷新CDN缓存！TaskId: ' + res.Response.TaskId);
+  else console.error(res.Response.Error);
 });
